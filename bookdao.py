350c8ff9b4ecc6b4b0b27ffea87dd2beabe2fe69
Hugo-van-Zyl <hugoandrevanzyl@gmail.com>
2026-05-13 22:15:50 +0000

    create data access object py

---
+++ bookdao.py
@@ -0,0 +1,5 @@
+# data access object
+# basically all the sql stuff goes in here so the server file doesnt get messy
+# i went with sqlite3 instead of mysql cos you dont need wamp or anything running
+# got the idea from lab 06.2 but changed it to sqlite
+# https://docs.python.org/3/library/sqlite3.html
